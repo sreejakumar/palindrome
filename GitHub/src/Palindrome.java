6e4663ff7a0c657f4838ea0da3a33dbcd949012c
sreejakumar <mpskiran712@gmail.com>
2019-05-30 04:18:03 -0700

    Employee details in Palindrome

---
+++ GitHub/src/Palindrome.java
@@ -0,0 +1,27 @@
+
+public class Palindrome {
+	private int empno;
+	private String empname;
+	private long sal;
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public long getSal() {
+		return sal;
+	}
+	public void setSal(long sal) {
+		this.sal = sal;
+	}
+	
+	
+	
+}
