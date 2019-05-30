86118a0fb12d5f91c35029542aea93986e65da8e
sreejakumar <mpskiran712@gmail.com>
2019-05-30 04:25:14 -0700

    Testing Palindrome

---
+++ GitHub/src/TPalindrome.java
@@ -0,0 +1,9 @@
+
+public class TPalindrome {
+	public static void main(String[] args) {
+		Palindrome p=new Palindrome();
+		p.setEmpname("SrejaKiran");
+		p.setEmpno(100);
+		p.setSal(200000);
+	}
+}
